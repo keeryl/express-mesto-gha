ee6bc18dae4cfa5c3367aa5f5b5b27336ebcfabf
Кирилл Зикевский <2309696@mail.ru>
2022-03-29 15:45:52 +0300

    dev: development in progress

--- utils/constants.js
+++ utils/constants.js
@@ -1,11 +1,11 @@
-const ERRROR_400 = 400;
+const ERROR_400 = 400;
 const ERROR_404 = 404;
 const ERROR_500 = 500;
 const JWT_SECRET = 'FmvkeTe6482902_scj%(&%%!$#&';
 const SALT_ROUNDS = 10;
 
 module.exports = {
-  ERRROR_400,
+  ERROR_400,
   ERROR_404,
   ERROR_500,
   JWT_SECRET,
